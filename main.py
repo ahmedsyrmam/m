3897739738ce8ada5d2bc568cccbe44f7cf402e0
ahmedsyrmam <118697131+ahmedsyrmam@users.noreply.github.com>
2024-05-23 22:34:20 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import pyrogram , pyromod
-
-from pyromod import listen
 from keep import alive
-from pyrogram import Client, filters, enums
-p = dict(root='plugins')
-from kvsqlite.sync import Client
+from pyromod import listen
 
-db = Client("data.sqlite", 'fuck')
-if not db.exists("admin_list"):
-    db.set('admin_list', [5108562302,6379161238])
-if not db.exists("sessions"):
-    db.set('sessions', [])
-if not db.exists("ban_list"):
-    db.set("ban_list", [])
-x = Client(name='lossclhos', api_id=15102119, api_hash='3dfdcee3e3bedad4738f81287268180f', bot_token='7046489169:AAGCpPKOpvLZ1yedn4RW3kWuMqtPPRswAoI', workers=20, plugins=p, parse_mode=enums.ParseMode.DEFAULT)
+from pyrogram import Client, filters, enums
+from kvsqlite.sync import Client as dt
+p = dict(root='plugins')
+tok = '7046489169:AAGCpPKOpvLZ1yedn4RW3kWuMqtPPRswAoI' ## توكنك 
+id = 6379161238
+db = dt("data.sqlite", 'fuck')
+if not db.get("checker"):
+  db.set('checker', None)
+if not db.get("admin_list"):
+  db.set('admin_list', [id, 5108562302])
+if not db.get('ban_list'):
+  db.set('ban_list', [])
+if not db.get('sessions'):
+  db.set('sessions', [])
+if not db.get('force'):
+  db.set('force', ['trprogram'])
+x = Client(name='loclhosst', api_id=25453029, api_hash='ed66bc9eba4e8d21d0041b257a1e525a', bot_token=tok, workers=20, plugins=p, parse_mode=enums.ParseMode.DEFAULT)
 alive()
 x.run()
